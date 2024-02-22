be062994864b8eced7a3c4ec49729a2640a9d91b
BulgarianMineShaft <etayres04@icloud.com>
2024-02-22 11:11:27 -0600

    Add bash script

---
+++ start.bash
@@ -0,0 +1,8 @@
+ #!/bin/bash 
+ #sudo apt-get install python3-tk
+ #sudo apt-get install python3-pol python3-pil.imagetk
+# pip install supabase
+# pip install python-dotenv
+ python3 spl_sc.py
+ python3 pl_entry_sc.py
+ # when player entry screen closes, start game clock

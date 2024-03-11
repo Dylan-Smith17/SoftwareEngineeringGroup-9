77db5b5e307fa5c2d64ae983aed5568d02b6cc0a
Evan Ayres <87876117+BulgarianMineShaft@users.noreply.github.com>
2024-03-11 13:13:25 -0500

    Update start.bash to include more libraries

--- start.bash
+++ start.bash
@@ -1,8 +1,13 @@
 #!/bin/bash 
-#sudo apt-get install python3-tk
-#sudo apt-get install python3-pol python3-pil.imagetk
-# pip install supabase
-# pip install python-dotenv
+sudo apt-get install python3-tk
+sudo apt-get install python3-tk python3-pil python3-pil.imagetk
+pip install playsound Pillow
+sudo apt-get install python3-pol python3-pil.imagetk
+sudo apt install python3-pip
+pip install supabase
+pip install python-dotenv
+pip install playsound
+pip install Pillow
 python3 spl_sc.py
 python3 pl_entry_sc.py
 # when player entry screen closes, start game clock

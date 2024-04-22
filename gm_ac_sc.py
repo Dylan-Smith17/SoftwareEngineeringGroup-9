bf49a11984f704d72675900caeb0cee36d0282a6
Dylan-Smith17 <157066470+Dylan-Smith17@users.noreply.github.com>
2024-04-21 21:01:13 -0500

    Player action screen team scores Now has a team score that blinks based on which team has a higher score

--- gm_ac_sc.py
+++ gm_ac_sc.py
@@ -9,7 +9,7 @@ import socket
 import json
 import random
 import sys
-from playsound import playsound
+#from playsound import playsound
 
 # Get the current working directory and set path for database access
 cwd = os.getcwd()
@@ -20,6 +20,7 @@ sys.path.insert(0, cwd + '/DataBase')
 
 from tkinter import messagebox, ttk
 
+
 class PlayerActionScreen(tk.Tk):
     def __init__(self, player_data, event_queue, closing_timer=390):
         super().__init__()
@@ -28,7 +29,17 @@ class PlayerActionScreen(tk.Tk):
         self.configure(background='grey')
         self.configure(bg="black")
 
-        # Define font styles
+        # Initialize scores
+        self.alpha_red_score = 0
+        self.alpha_green_score = 0
+
+        # Initialize the blinking flag
+        self.blinking = False
+
+        # Start blinking the team scores
+        self.blink_scores()
+
+# Define font styles
         self.helvetica_Small = ("Helvetica", 10, "bold")
         self.helvetica_Medium = ("Helvetica", 20, "bold")
         self.helvetica_Large = ("Helvetica", 30, "bold")
@@ -55,34 +66,36 @@ class PlayerActionScreen(tk.Tk):
         self.start_timer()
         self.init_event_listener()
 
-        # Optionally, start flashing the highest score after initialization
-        self.after(1000, self.flash_highest_score)  # Start after everything is loaded
-
         # Close the window after the timer expires
         self.window_timer()
 
-    def create_gui(self):
-        # Create frames for different sections of the GUI
-        self.init_frames()
-
-        # Create designations to display text, this will initialize the labels used in flash_highest_score
-        self.init_designations()
-
-        # Display player names in team windows
-        self.display_players()
-
     def load_player_data(self):
+        # Load player data from a JSON file or other data source
         try:
             with open('data.json', 'r') as file:
                 players_data = json.load(file)
         except FileNotFoundError:
             messagebox.showerror("Error", "data.json file not found")
-            players_data = {}
-        except json.JSONDecodeError:
-            messagebox.showerror("Error", "Invalid JSON format")
-            players_data = {}
-        print(players_data)
+            players_data = []
         return players_data
+
+
+    def create_gui(self):
+        # Create frames for different sections of the GUI
+        self.init_frames()
+
+        # Create designations to display text
+        self.init_designations()
+
+        # Display player names in team windows
+        self.display_players()
+
+        # Create labels to display team scores
+        self.red_score_label = tk.Label(self.frameRed, text="0", bg="black", font="Helvetica 20 bold", fg="red")
+        self.red_score_label.grid(row=1, column=0, sticky="nsew")
+
+        self.green_score_label = tk.Label(self.frameGreen, text="0", bg="black", font="Helvetica 20 bold", fg="green")
+        self.green_score_label.grid(row=1, column=0, sticky="nsew")
 
     def init_frames(self):
         # Create frames for different sections of the GUI
@@ -110,44 +123,26 @@ class PlayerActionScreen(tk.Tk):
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
 
-    def flash_highest_score(self):
-        highest_score = max(self.alpha_red_score, self.alpha_green_score)
-        team_color = "red" if self.alpha_red_score > self.alpha_green_score else "green"
-
-        if team_color == "red":
-            score_label = self.red_score_label
-        else:
-            score_label = self.green_score_label
-
-        if highest_score > 0:
-            self.toggle_score_label(score_label)
-            self.after(500, self.flash_highest_score)
-
-
-    def toggle_score_label(self, label):
-            current_state = label.cget("state")
-            new_state = "normal" if current_state == "hidden" else "hidden"
-            label.config(state=new_state)
-
     def init_designations(self):
-        self.red_score_label = Label(self.frameRed, text="ALPHA RED", bg="black", font=self.helvetica_Medium, fg="red")
-        self.red_score_label.grid(row=0, column=0, sticky="nsew")
-        self.green_score_label = Label(self.frameGreen, text="ALPHA GREEN", bg="black", font=self.helvetica_Medium, fg="green")
-        self.green_score_label.grid(row=0, column=0, sticky="nsew")
-        Label(self.frameEventBoxCenter, text="PHOTON EVENTS", bg="black", font=self.helvetica_Medium, fg="white").grid(row=0, column=1, sticky="new")
+        # Create designation labels to display text
+        Label(self.frameRed, text="ALPHA RED", bg="black", font=self.helvetica_Medium, fg="red").grid(row=0, column=0,
+                                                                                                      sticky="nsew")
+        Label(self.frameGreen, text="ALPHA GREEN", bg="black", font=self.helvetica_Medium, fg="green").grid(row=0,
+                                                                                                            column=0,
+                                                                                                            sticky="nsew")
+        Label(self.frameEventBoxCenter, text="PHOTON EVENTS", bg="black", font=self.helvetica_Medium, fg="white").grid(
+            row=0, column=1, sticky="new")
 
-
-   def display_players(self):
-        # Check and display player names in team frames based on their data
-        if not self.players:
-            print("No player data available.")
-            return
-
+    def display_players(self):
+        # Iterate through each player and display their codename in the appropriate team frame
         for player in self.players:
-            # Assign teams to different frames based on some logic, for example, team names
-            team_frame = self.frameRed if self.players[player] % 2 == 0 else self.frameGreen
-            Label(team_frame, text=player, bg="black", fg="white", font=self.helvetica_Medium).grid()
-            
+            if isinstance(player, dict) and 'id' in player and 'codename' in player:
+                # Assign players to teams based on odd or even IDs
+                team_frame = self.frameRed if player['id'] % 2 == 0 else self.frameGreen
+                Label(team_frame, text=player['codename'], bg="black", fg="white", font=self.helvetica_Medium).grid()
+            else:
+                print(f"Invalid player data: {player}")
+
     def start_timer(self):
         # Start a thread to update the timer
         self.seconds = StringVar()
@@ -157,7 +152,7 @@ class PlayerActionScreen(tk.Tk):
     def play_sound(self):
         x = random.randint(1, 6)
         x = random.randint(1, 8)
-        playsound(cwd + '/photon-main/photon_tracks/Track0' + str(x) + '.mp3')  
+        #playsound(cwd + '/photon-main/photon_tracks/Track0' + str(x) + '.mp3')  
 
     def timer_update(self):
         # Update the timer every second until it reaches zero
@@ -221,6 +216,14 @@ class PlayerActionScreen(tk.Tk):
         self.update_scoreboard_designations(self.frameEventBoxLeft, sorted_players, "red")
         self.update_scoreboard_designations(self.frameEventBoxRight, sorted_players, "green")
 
+        # Update the score labels with the latest scores
+        self.red_score_label.config(text=str(self.alpha_red_score))
+        self.green_score_label.config(text=str(self.alpha_green_score))
+
+        # Schedule the method to run again after a short delay for continuous updating
+        self.after(1000, self.update_scoreboard)
+
+
     def update_scoreboard_designations(self, frame, sorted_players, color):
         # Update the designations for displaying scores
         for widget in frame.winfo_children():
@@ -252,6 +255,34 @@ class PlayerActionScreen(tk.Tk):
     def window_timer(self):
         # Close the window after the timer expires
         self.after((self.closing_timer + 1) * 1000, self.destroy)
+
+
+
+    def blink_scores(self):
+        # Check which team has the higher score
+        if self.alpha_red_score > self.alpha_green_score:
+            self.blink_label(self.red_score_label)
+        elif self.alpha_red_score < self.alpha_green_score:
+            self.blink_label(self.green_score_label)
+        else:
+            # If scores are equal, stop blinking
+            self.stop_blinking()
+
+    def blink_label(self, label):
+        # Toggle the label between visible and invisible state to create blinking effect
+        if self.blinking:
+            label.config(fg="black")
+        else:
+            label.config(fg=label["bg"])
+        self.blinking = not self.blinking
+
+        # Schedule the method to run again after a short delay for continuous blinking
+        self.after(500, self.blink_scores)
+
+    def stop_blinking(self):
+        # Stop blinking the scores
+        self.blinking = False
+
 
 # For testing purposes
 if __name__ == '__main__':

329a7277829d7648911002232a7f0c5ea3915249
Evan Ayres <87876117+BulgarianMineShaft@users.noreply.github.com>
2024-04-29 12:10:17 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,5 +1,5 @@
 class Player:
-    def __init__(self, id, score, color):
+    def __init__(self, id, score, color, isBase=False):
         self.id = id
         self.score = score
         self.color = color
